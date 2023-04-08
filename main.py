e21b1c5e0faff635d6c32b1e933aabac7d61aff6
AniruddhA-Omni <aniruddha.jana2020@vitstudent.ac.in>
2023-04-08 12:46:57 +0530

    Removed Randomforest algo & Added KMeans algo

--- main.py
+++ main.py
@@ -19,24 +19,12 @@ except ValueError as e:
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://plant2iot-default-rtdb.firebaseio.com/'})
 
-##################################### Converting to dataframe and adding condition column ############################
-def condition(x):
-    if x["Moisture"] > 700:
-        if x["Temp"] >= 33:
-            return "Unbearable"
-        return "Dry"
-    elif x["Moisture"] < 450:
-        return "Moist"
-    else:
-        return "Healthy"
-
-
 ################################################ Web App ###########################################################
 st.set_page_config(page_title='Analytics App', layout='centered', initial_sidebar_state='auto', page_icon="🌱")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title("Plant2IoT Analytics App")
-st.write("This is a web app for analytics of Plant2IoT project. The data is fetched from the Firebase Realtime Database. The data is then converted to a dataframe and then the data is visualized using Seaborn and Matplotlib.")
+st.write("This is a web app for analytics of Plant2IoT project. The data is fetched from the Firebase Realtime Database. The data is then converted to a dataframe and then the data is visualized using Seaborn and Matplotlib. The data is also used for Machine Learning using KMeans Clustering and PCA. And the model is generated and can be saved using Pickle. The model can be used for prediction of the plant's health.")
 st.markdown("""
 #### **Current data and time:**""")
 today = datetime.now()
@@ -48,9 +36,6 @@ ds = ref_iot.get()
 ref_count = db.reference('/count/')
 ds2 = ref_count.get()+1
 df = pd.DataFrame(ds)
-df["Condition"] = df.apply(condition, axis=1)
-dx = pd.DataFrame()
-dx = df
 
 st.markdown("""
 
@@ -68,31 +53,31 @@ st.markdown(
 )
 
 def diff(x):
-    return str(dx[x].iloc[-1] - dx[x].iloc[-2])
+    return str(df[x].iloc[-1] - df[x].iloc[-2])
 
 kpi1, kpi2, kpi3, kpi4 = st.columns(4)
 
 kpi1.metric(
     label="🌡️ Temperature (°C)",
-    value=str(dx["Temp"].iloc[-1]),
+    value=str(df["Temp"].iloc[-1]),
     delta=diff("Temp"),
 )
 
 kpi2.metric(
     label="💧 Soil Moisture",
-    value= str(dx["Moisture"].iloc[-1]),
+    value= str(df["Moisture"].iloc[-1]),
     delta=diff("Moisture"),
 )
 
 kpi3.metric(
     label="🌤️ Light Intensity",
-    value=str(dx["Light"].iloc[-1]),
+    value=str(df["Light"].iloc[-1]),
     delta=diff("Light"),
 )
 
 kpi4.metric(
     label="🌫️ Humidity (%)",
-    value=str(dx["Hum"].iloc[-1]),
+    value=str(df["Hum"].iloc[-1]),
     delta=diff("Hum"),
 )
 st.markdown("""
@@ -105,8 +90,6 @@ if st.button("Fetch Data from Firebase"):
     ref_count = db.reference('/count/')
     ds2 = ref_count.get()+1
     df = pd.DataFrame(ds)
-    df["Condition"] = df.apply(condition, axis=1)
-    dx = df
     st.write("Fetching data from Firebase...")
     st.write("Data fetched successfully!")
     st.write("Total rows:",ds2)
@@ -127,34 +110,34 @@ def plot_analytics():
     sns.set_theme(style="darkgrid")
     st.markdown("""**1.** The following plot shows the **Soil Moisture** values of the plant.""")
     fig, ax = plt.subplots()
-    ax.plot(dx.index.values , dx["Moisture"],".-g",ms = 7, mfc="k")
+    ax.plot(df.index.values , df["Moisture"],".-g",ms = 7, mfc="k")
     plt.xlabel("Time")
     plt.ylabel("Moisture")
     st.pyplot(fig)
     
     st.markdown("""**2.** The following plot shows the **Temperature** values of the plant.""")
     fig, ax = plt.subplots()
-    ax.plot(dx.index.values , dx["Temp"],".-r",ms = 7, mfc="g")
+    ax.plot(df.index.values , df["Temp"],".-r",ms = 7, mfc="g")
     plt.xlabel("Time")
     plt.ylabel("Temperature")
     st.pyplot(fig)
     
     st.markdown("""**3.** The following plot shows the **Humidity** values of the plant.""")
     fig, ax = plt.subplots()
-    ax.plot(dx.index.values , dx["Hum"],".-b",ms = 7, mfc="r")
+    ax.plot(df.index.values , df["Hum"],".-b",ms = 7, mfc="r")
     plt.xlabel("Time")
     plt.ylabel("Humidity")
     st.pyplot(fig)
     
     st.markdown("""**4.** The following plot shows the **Light** values of the plant.""")
     fig, ax = plt.subplots()
-    ax.plot(dx.index.values , dx["Light"],".-k",ms = 7, mfc="gray")
+    ax.plot(df.index.values , df["Light"],".-k",ms = 7, mfc="gray")
     plt.xlabel("Time")
     plt.ylabel("Light Intensity")
     st.pyplot(fig)
     
     st.markdown("""**5.** The Correlation Matrix of the data is shown below.""")
-    corr = dx.corr()
+    corr = df.corr()
     fig, ax = plt.subplots()
     sns.heatmap(corr, annot=True, ax=ax)
     st.write(fig)
@@ -162,65 +145,77 @@ def plot_analytics():
 if st.button("Plot Analytics"):
     plot_analytics()
     st.markdown("""**6.** The latest data is shown below.""")
-    st.write(dx.tail())
-    st.write("Last Condition:",dx["Condition"].iloc[-1])
+    st.write(df.tail())
     
 ########################################################## Machine Learning #########################################
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
 
-X = dx.drop(["Condition"], axis=1)
-y = dx["Condition"]
 
 st.subheader("Machine Learning")
 st.markdown("""
-**1.** The following model is used to predict the condition of the plant.
+**1.** The following model is used to cluster the unnsupervised data.
 
-**2.** Random Forest Classifier is used.
+**2.** K Means Clustering is used.
 
 **3.** The model is trained on the data collected from the plant.
 
-**4.** The model is then tested on the data collected from the plant.
+**4.** The model is then used to predict the condition of the plant.
 
-**5.** The accuracy of the model is shown below.
+**5.** Different clusters are formed based on the data.
 """)
 
 def model_gen():
-    model = RandomForestClassifier()
+    data = df
+    inertias = []
+    
+    pcs = PCA(n_components=2)
+    pcs.fit(data)
+    datap = pcs.transform(data)
 
-    model.fit(X, y)
+    for i in range(1,11):
+        kmeans = KMeans(n_clusters=i)
+        kmeans.fit(datap)
+        inertias.append(kmeans.inertia_)
 
-    y_pred = model.predict(X)
-
-    st.write("Accuracy:",model.score(X, y)*100)
-    #############################
-
-    st.write("Confusion Matrix:")
-    # Get and reshape confusion matrix data
-    matrix = confusion_matrix(y, y_pred)
-
-    # Build the plot
     fig, ax = plt.subplots()
-    sns.set(font_scale=1.4)
-    sns.heatmap(matrix, annot=True,
-                cmap=plt.cm.Greens, linewidths=0.2, ax=ax)
-    # Add labels to the plot
-    class_names = ['Dry', 'Healthy', 'Moist']
-    tick_marks = np.arange(len(class_names))
-    tick_marks2 = tick_marks + 0.5
-    plt.xticks(tick_marks, class_names, rotation=25)
-    plt.yticks(tick_marks2, class_names, rotation=0)
-    plt.xlabel('Predicted label')
-    plt.ylabel('True label')
-    plt.title('Confusion Matrix for Random Forest Model')
+    ax.plot(range(1,11), inertias, marker='o')
+    plt.title('Elbow method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
     st.pyplot(fig)
-    #######################################
-    report = classification_report(y, y_pred, output_dict=True)
-    rp = pd.DataFrame(report).transpose()
-    st.write("Classification Report", rp)
+    st.markdown("""
+    Elbow method is used to find the optimal number of clusters.
+    
+    The optimal number of clusters is **3**.
+    
+    These clusters are {0:"Moist", 1:"Dry", 2:"Healthy"}
+    """)
+    model = KMeans(n_clusters=3, init='k-means++',random_state=69)
+    model.fit(datap)
 
-    ######################################
+    dfk = pd.concat([data.reset_index(drop=True), pd.DataFrame(datap, columns=['PC1', 'PC2'])], axis=1)
+    dfk['Cluster'] = model.labels_
+
+    st.write("The clusters are shown below.")
+    st.write(dfk.head())
+    st.markdown("""
+    After clustering and analyzing the data, the following clusters are formed.
+    A new column is added to the dataframe which shows the condition of the plant.
+    """)
+
+    d = {0:"Moist", 1:"Dry", 2:"Healthy"}
+    
+    dfk["Condition"] = dfk["Cluster"].map(d)
+    
+    st.table(pd.crosstab(dfk["Cluster"], dfk["Condition"]))
+    
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=dfk, x='PC1', y='PC2', hue='Condition', palette='Set1', ax=ax)
+    plt.title('Clusters')
+    st.pyplot(fig)
+    
     def dowload_model(model):
         output_model = pickle.dumps(model)
         b64 = base64.b64encode(output_model).decode()
@@ -228,27 +223,34 @@ def model_gen():
         return href
     st.markdown(dowload_model(model), unsafe_allow_html=True)
     
+    st.markdown(dowload_file(dfk), unsafe_allow_html=True)
+
+    ######################################
+    
+    
 if st.button("Generate Model"):
     model_gen()
 
 ########################################################## Conclusion ################################################
+#### Pipeline ####
+pipe = Pipeline([('pca', PCA(n_components=2)),
+                 ('kmeans', KMeans(n_clusters=3, init='k-means++',random_state=69))])
+pipe.fit(df)
+result = pipe.predict(df.iloc[-1].values.reshape(1,-1))
+
 st.subheader("Conclusions")
 st.markdown("""
-**1.** The plant is **healthy** when the moisture value is between 450 and 700.
+    **1.** The following model is used to cluster the unnsupervised data.
+            
+    **2.** The optimal number of clusters is **3**.
+    
+    **3.** These clusters are {0:"Moist", 1:"Dry", 2:"Healthy"}
 
-**2.** The plant is **dry** when the moisture value is greater than 700.
-
-**3.** The plant is **moist** when the moisture value is less than 450.
-
-**4.** The plant is **unbearable** when the moisture value is greater than 700 and the temperature value is greater than or equal to 33.
-
-""")
+    """)
 if st.button("View Suggestions"):
-    if dx["Condition"].iloc[-1] == "Unbearable":
-        st.write("The plant is unbearable. Please water the plant.")
-    elif dx["Condition"].iloc[-1] == "Dry":
-        st.write("The plant is dry. Please water the plant.")
-    elif dx["Condition"].iloc[-1] == "Moist":
-        st.write("The plant is moist. Please water the plant.")
+    if result == 1:
+        st.markdown("""#### The plant is dry. Please water the plant.""")
+    elif result == 0:
+        st.markdown("""#### The plant is moist. No need to water the plant.""")
     else:
-        st.write("The plant is healthy. No need to water the plant.")
+        st.markdown("""#### The plant is healthy. No need to water the plant.""")
